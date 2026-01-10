ca7c770a456c92d498b01e8386f0f5aabe5d7163
AkashKumarMallick <akashkumar@5bix.in>
2026-01-10 17:57:50 +0530

    fix: Disabled OHIF about popup

--- platform/ui-next/src/components/Header/Header.tsx
+++ platform/ui-next/src/components/Header/Header.tsx
@@ -83,8 +83,8 @@ function Header({
           <div className="absolute right-0 top-1/2 flex -translate-y-1/2 select-none items-center">
             {UndoRedo}
             <div className="border-primary-dark mx-1.5 h-[25px] border-r"></div>
-            {PatientInfo}
-            <div className="border-primary-dark mx-1.5 h-[25px] border-r"></div>
+            {/* {PatientInfo} */}
+            {/* <div className="border-primary-dark mx-1.5 h-[25px] border-r"></div> */}
             <div className="flex-shrink-0">
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
